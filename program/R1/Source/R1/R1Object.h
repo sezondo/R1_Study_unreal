4a2d4961172ec8df35af410bdae12380a737cfd5
sezondo <psdd205050@gmail.com>
2025-04-22 23:41:32 +0900

    Log 카테고리 추가 및 GC 실습 및 확인

---
+++ program/R1/Source/R1/R1Object.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "R1Object.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R1_API UR1Object : public UObject
+{
+	GENERATED_BODY()
+	
+
+public:
+		
+	int32 Hp = 100;
+	int32 Mp = 50;
+
+};
